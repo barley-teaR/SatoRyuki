4cb091b54699b6243a13e68942bbac650496748f
佐藤　龍季 <73323309+barley-teaR@users.noreply.github.com>
2021-06-25 13:07:37 +0900

    Add files via upload

---
+++ ロケパンファイターズ/PlayerUI/PlayerBanner/PlayerBanner.cpp
@@ -0,0 +1,10 @@
+#include"PlayerBanner.h"
+#include"../../../Managers/ResouceManager/ResouceManager.h"
+
+PlayerBanner::PlayerBanner()
+{
+}
+
+PlayerBanner::~PlayerBanner()
+{
+}
---
+++ ロケパンファイターズ/PlayerUI/PlayerBanner/PlayerBanner.h
@@ -0,0 +1,13 @@
+#pragma once
+#include"../../../../ESGLib.h"
+
+class PlayerBanner
+{
+public:
+	PlayerBanner();
+	~PlayerBanner();
+
+private:
+
+};
+
